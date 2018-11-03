681ea7ac0648d22c2fe5bc826baa42160b8cf018
Abhishek Chatterjee <abhishekchatterjeejit@gmail.com>
2018-11-03 19:28:12 +0530

    Created the web part

---
+++ script.js
@@ -0,0 +1,55 @@
+var canvas = document.querySelector( 'canvas' ),
+c = canvas.getContext( '2d' ),
+mouseX = 0,
+mouseY = 0,
+width = 300,
+height = 300,
+colour = 'white',
+mousedown = false;
+
+canvas.width = width;
+canvas.height = height;
+
+function draw() {
+    if (mousedown) {
+        c.fillStyle = colour; 
+        c.beginPath();
+        c.arc( mouseX, mouseY, 10 , 0, Math.PI*2, true );
+        c.closePath();
+        c.fill();
+    }
+}
+
+canvas.addEventListener( 'mousemove', function( event ) {
+        if( event.offsetX ){
+            mouseX = event.offsetX;
+            mouseY = event.offsetY;
+        } else {
+            mouseX = event.pageX - event.target.offsetLeft;
+            mouseY = event.pageY - event.target.offsetTop;
+        }
+        draw();
+    }, false );
+
+canvas.addEventListener( 'mousedown', function( event ) {
+    mousedown = true;
+}, false );
+canvas.addEventListener( 'mouseup', function( event ) {
+    mousedown = false;
+}, false );
+
+
+
+
+
+
+
+
+// var link = document.createElement('a');
+//     link.innerHTML = 'download image';
+// link.addEventListener('click', function(ev) {
+//     console.log(canvas.toDataURL());
+//     // link.href = canvas.toDataURL();
+//     // link.download = "mypainting.png";
+// }, false);
+// document.body.appendChild(link);
